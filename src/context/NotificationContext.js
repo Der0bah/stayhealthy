a59dbddf9ae42cbc01cb13bf5f621a1773191e38
Der0bah <derobah@me.com>
2025-08-24 18:51:12 -0300

    Update NotificationContext.js

--- src/context/NotificationContext.js
+++ src/context/NotificationContext.js
@@ -1,21 +1,8 @@
 import React, { createContext, useContext } from "react";
 
-/**
- * Provides global notification controls:
- *  - show(appointmentObject)
- *  - hide()
- */
-export const NotificationContext = createContext({
-  show: (_appointment) => {},
-  hide: () => {},
-});
-
+export const NotificationContext = createContext({ show: () => {}, hide: () => {} });
 export const useNotification = () => useContext(NotificationContext);
 
 export default function NotificationProvider({ value, children }) {
-  return (
-    <NotificationContext.Provider value={value}>
-      {children}
-    </NotificationContext.Provider>
-  );
+  return <NotificationContext.Provider value={value}>{children}</NotificationContext.Provider>;
 }
